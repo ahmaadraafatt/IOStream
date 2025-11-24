1103bf6a3b83399bf50fd53e0a024823761ff996
ahmed raafat <ahmedrafat@gmail.com>
2025-11-24 02:18:34 +0200

    Add project files.

---
+++ CAFileStream/Program.cs
@@ -0,0 +1,147 @@
+﻿namespace CAFileStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example10();
+            Console.ReadKey();
+        }
+        static void Example01()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown.txt";
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                Console.WriteLine($"Length: {fs.Length}");
+                Console.WriteLine($"CanRead: {fs.CanRead}");
+                Console.WriteLine($"CanWrite: {fs.CanWrite}");
+                Console.WriteLine($"CanSeek: {fs.CanSeek}");
+                Console.WriteLine($"CanTimeout: {fs.CanTimeout}");
+                Console.WriteLine($"Position: {fs.Position}");
+                fs.WriteByte(65);
+                fs.WriteByte(66);
+                fs.WriteByte(13);
+                Console.WriteLine($"Position: {fs.Position}");
+                for (byte i = 65; i < 123; i++)
+                {
+                    fs.WriteByte(i);
+                }
+
+            }
+        }
+        static void Example02()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown.txt";
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                byte[] data = new byte[fs.Length];
+                int numBytesToRead = (int)fs.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = fs.Read(data, numBytesRead, numBytesToRead);
+                    if (n == 0)
+                        break;
+
+                    numBytesToRead -= n;
+                    numBytesRead += n;
+                }
+
+                foreach (var b in data)
+                    Console.WriteLine(b);
+
+
+                var newPath = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown1.txt";
+                using (var fsw = new FileStream(newPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    fsw.Write(data, 0, data.Length);
+                }
+            }
+
+        }
+        static void Example03()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown2.txt";
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                fs.Seek(20, SeekOrigin.Begin);
+                fs.WriteByte(65);
+                fs.Position = 0;
+                for (int i = 0; i < fs.Length; i++)
+                {
+                    Console.WriteLine(fs.ReadByte());
+                }
+            }
+        }
+
+        static void Example04()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown3.txt";
+
+            using (var sw = new StreamWriter(path))
+            {
+                sw.WriteLine("This");
+                sw.WriteLine("is");
+                sw.WriteLine("C#");
+            }
+        }
+        static void Example05()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown3.txt";
+            using (var sr = new StreamReader(path))
+            {
+                while (sr.Peek() > 0)
+                {
+                    Console.Write((char)sr.Read());
+                }
+            }
+        }
+        static void Example06()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown3.txt";
+            using (var sr = new StreamReader(path))
+            {
+                string line;
+                while((line = sr.ReadLine()) is not null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        static void Example07()
+        {
+           string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown4.txt";
+
+            string[] lines =
+            {
+                "C#",
+                "is",
+                "Amazing",
+                "Language"
+            };
+            File.WriteAllLines(path, lines);
+
+        }
+        static void Example08() 
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown5.txt";
+            string text = "C# is Amazing Language";
+            File.WriteAllText(path, text);
+
+        }
+        static void Example09()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown5.txt";
+            var result = File.ReadAllText(path);
+            Console.WriteLine(result);
+        }
+        static void Example10()
+        {
+            string path = "C:\\Users\\ahmes\\OneDrive\\Desktop\\unnown4.txt";
+            var result = File.ReadAllLines(path);
+            foreach(var line in result)
+                Console.WriteLine(line);
+           
+        }
+    }
+}
---
+++ CAIDisposapleDesignPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace CAIDisposapleDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)not recommended
+            //CurrencyService currencyServicedService = new CurrencyService();
+            //var result = currencyServicedService.GerCurrencies();
+            //currencyServicedService.Dispose();
+            //Console.WriteLine(result);
+
+            // 2) Recommended
+            //CurrencyService currencyServicedService = null;
+            //try
+            //{
+            //    currencyServicedService = new CurrencyService();
+            //    var result = currencyServicedService.GerCurrencies();
+            //    Console.WriteLine(result);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Error");
+            //}
+            //finally
+            //{
+            //    currencyServicedService?.Dispose();
+            //}
+
+            //// 3) more recommmended using .net framework 2+
+            //using (CurrencyService currencyServicedService = new CurrencyService()) 
+            //{
+
+            //    var result = currencyServicedService.GerCurrencies();
+            //    Console.WriteLine(result);
+            //}
+
+
+            // 4) using without blocks C# 8.0
+            using CurrencyService currencyServicedService = new CurrencyService();
+            var result = currencyServicedService.GerCurrencies();
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+        class CurrencyService : IDisposable
+        {
+            private readonly HttpClient _httpClient;
+            private bool _disposed = false;
+            public CurrencyService()
+            {
+                _httpClient = new HttpClient();
+            }
+
+            ~CurrencyService()
+            {
+                Dispose(false);
+            }
+            //disposing : true (dispose manged + unmanaged)
+            //disposing : true (dispos unmanaged + large fields)
+            protected virtual void Dispose(bool disposing)
+            {
+                if (_disposed)
+                    return;
+                if (disposing)
+                {
+                    _httpClient.Dispose();
+                }
+            }
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            public string GerCurrencies()
+            {
+                string url = "https://coinbase.com/api/v2/currencies";
+                var result = _httpClient.GetStringAsync(url).Result;
+                return result;
+            }
+        }
+    }
+}
